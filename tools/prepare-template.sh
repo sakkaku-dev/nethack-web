a90ea007f84b541e500d5cc9f3c33369bc5643c1
myin <minyin142p@gmail.com>
2023-04-27 14:48:28 +0200

    add release workflow

---
+++ tools/prepare-template.sh
@@ -0,0 +1,10 @@
+VERSION=${GODOT_VERSION:-$1}
+TARGET=~/.local/share/godot/templates/${VERSION}.stable
+
+if [ -f "$TARGET" ]; then
+    echo "Templates already installed"
+    exit
+fi
+
+mkdir -v -p ~/.local/share/godot/templates
+mv /root/.local/share/godot/templates/${VERSION}.stable $TARGET
