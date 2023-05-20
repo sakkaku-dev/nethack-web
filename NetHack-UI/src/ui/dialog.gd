3bce7800ab37d52e4e9ce54c19ec0c69c11d1c5e
myin <minyin142p@gmail.com>
2023-05-20 21:50:34 +0200

    clean up
    skip-checks: true

--- NetHack-UI/src/ui/dialog.gd
+++
@@ -1,6 +0,0 @@
-extends CenterContainer
-
-@export var text_node: RichTextLabel
-
-func open(text: String):
-	text_node.text = text
--- NetHack-UI/src/utils.gd
+++
@@ -1,8 +0,0 @@
-class_name Utils
-
-const TILE_SIZE_X = 40
-
-static func to_tilev(tile: int) -> Vector2:
-	var row = floor(tile / float(TILE_SIZE_X))
-	var col = tile % TILE_SIZE_X
-	return Vector2(col, row)
