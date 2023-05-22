22196100a265066960d851b084570136f4f8ebcf
myin <minyin142p@gmail.com>
2023-05-22 09:01:47 +0200

    show glyph in select menu (close #16)

--- src/ui/styles.ts
+++ src/ui/styles.ts
@@ -12,8 +12,30 @@ export function topLeft(elem: HTMLElement) {
     elem.style.left = '0';
 }
 
+export function topRight(elem: HTMLElement) {
+    elem.style.position = 'absolute';
+    elem.style.top = '0';
+    elem.style.right = '0';
+}
+
 export function center(elem: HTMLElement) {
     elem.style.display = 'flex';
     elem.style.justifyContent = 'center';
     elem.style.alignItems = 'center';
+}
+
+export function horiz(elem: HTMLElement) {
+    elem.style.display = 'flex';
+    elem.style.gap = '0.5rem';
+    elem.style.alignItems = 'center';
+}
+
+export function rel(elem: HTMLElement) {
+    elem.style.position = 'relative';
+}
+
+export function accelStyle(elem: HTMLElement) {
+    topRight(elem)
+    elem.style.background = '#00000099';
+    elem.style.padding = '0 0.1rem';
 }
