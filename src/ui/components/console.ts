5d8c2750596a53e405e8cdc52e30d161a214e9a8
myin <minyin142p@gmail.com>
2023-06-06 16:24:51 +0200

    move console to top (closes #70)

--- src/ui/components/console.ts
+++ src/ui/components/console.ts
@@ -5,10 +5,23 @@ export class Console {
         this.elem = document.createElement('pre');
         this.elem.id = 'output';
         root.appendChild(this.elem);
+
+        this.elem.onclick = () => {
+            if (this.elem.style.height) {
+                this.elem.style.height = '';
+                setTimeout(() => this.scrollBottom(), 200); // Wait until transition finished
+            } else {
+                this.elem.style.height = '25rem';
+            }
+        }
     }
 
     appendLine(line: string) {
         this.elem.innerHTML += line + '\n';
+        this.scrollBottom();
+    }
+
+    private scrollBottom() {
         this.elem.scrollTo(0, this.elem.scrollHeight);
     }
 }
