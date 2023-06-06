cf7f384d148303d2d95ccbfc3d061daadf66b2b1
myin142 <minyin142p@gmail.com>
2023-06-06 14:25:48 +0000

    Deploying to gh-pages from @ sakkaku-dev/nethack-web@5d8c2750596a53e405e8cdc52e30d161a214e9a8 🚀

--- nethack-ui.js
+++ nethack-ui.js
@@ -1297,9 +1297,21 @@ class Console {
         this.elem = document.createElement('pre');
         this.elem.id = 'output';
         root.appendChild(this.elem);
+        this.elem.onclick = () => {
+            if (this.elem.style.height) {
+                this.elem.style.height = '';
+                setTimeout(() => this.scrollBottom(), 200); // Wait until transition finished
+            }
+            else {
+                this.elem.style.height = '25rem';
+            }
+        };
     }
     appendLine(line) {
         this.elem.innerHTML += line + '\n';
+        this.scrollBottom();
+    }
+    scrollBottom() {
         this.elem.scrollTo(0, this.elem.scrollHeight);
     }
 }
